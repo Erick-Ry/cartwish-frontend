3444870fe242337fa8e7e67c77ba36e4fdb0df9d
ErickRyan~ <erickryanperez@gmail.com>
2025-01-21 14:37:54 +0800

    modify cart module

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,5 +1,5 @@
 import React, { memo, useContext, useMemo } from "react";
-
+import config from "../../config.json";
 import UserContext from "../../contexts/UserContext";
 import "./CartPage.css";
 import remove from "../../assets/remove.png";
@@ -10,95 +10,99 @@ import { checkoutAPI } from "../../services/orderServices";
 import { toast } from "react-toastify";
 
 const CartPage = () => {
-  const user = useContext(UserContext);
-  const { cart, removeFromCart, updateCart, setCart } = useContext(CartContext);
+    const user = useContext(UserContext);
+    const { cart, removeFromCart, updateCart, setCart } =
+        useContext(CartContext);
 
-  const subTotal = useMemo(() => {
-    let total = 0;
-    cart.forEach((item) => {
-      total += item.product.price * item.quantity;
-    });
+    const subTotal = useMemo(() => {
+        let total = 0;
+        cart.forEach((item) => {
+            total += item.product.price * item.quantity;
+        });
 
-    return total;
-  }, [cart]);
+        return total;
+    }, [cart]);
 
-  const checkout = () => {
-    const oldCart = [...cart];
-    setCart([]);
-    checkoutAPI()
-      .then(() => {
-        toast.success("Order placed successfully!");
-      })
-      .catch(() => {
-        toast.error("Something went wrong!");
-        setCart(oldCart);
-      });
-  };
+    const checkout = () => {
+        const oldCart = [...cart];
+        setCart([]);
+        checkoutAPI()
+            .then(() => {
+                toast.success("Order placed successfully!");
+            })
+            .catch(() => {
+                toast.error("Something went wrong!");
+                setCart(oldCart);
+            });
+    };
 
-  return (
-    <section className="align_center cart_page">
-      <div className="align_center user_info">
-        <img
-          src={`http://localhost:5000/profile/${user?.profilePic}`}
-          alt="user profile"
-        />
-        <div>
-          <p className="user_name">Name: {user?.name}</p>
-          <p className="user_email">Email: {user?.email}</p>
-        </div>
-      </div>
-
-      <Table headings={["Item", "Price", "Quantity", "Total", "Remove"]}>
-        <tbody>
-          {cart.map(({ product, quantity }) => (
-            <tr key={product._id}>
-              <td>{product.title}</td>
-              <td>${product.price}</td>
-              <td className="align_center table_quantity_input">
-                <QuantityInput
-                  quantity={quantity}
-                  stock={product.stock}
-                  setQuantity={updateCart}
-                  cartPage={true}
-                  productId={product._id}
-                />
-              </td>
-              <td>${quantity * product.price}</td>
-              <td>
+    return (
+        <section className="align_center cart_page">
+            <div className="align_center user_info">
                 <img
-                  src={remove}
-                  alt="remove icon"
-                  className="cart_remove_icon"
-                  onClick={() => removeFromCart(product._id)}
+                    src={`${config.backendURL}/profile/${user?.profilePic}`}
+                    alt="user profile"
                 />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+                <div>
+                    <p className="user_name">Name: {user?.name}</p>
+                    <p className="user_email">Email: {user?.email}</p>
+                </div>
+            </div>
 
-      <table className="cart_bill">
-        <tbody>
-          <tr>
-            <td>Subtotal</td>
-            <td>${subTotal}</td>
-          </tr>
-          <tr>
-            <td>Shipping Charge</td>
-            <td>$5</td>
-          </tr>
-          <tr className="cart_bill_final">
-            <td>Total</td>
-            <td>${subTotal + 5}</td>
-          </tr>
-        </tbody>
-      </table>
+            <Table headings={["Item", "Price", "Quantity", "Total", "Remove"]}>
+                <tbody>
+                    {cart.map(({ product, quantity }) => (
+                        <tr key={product._id}>
+                            <td>{product.title}</td>
+                            <td>${product.price}</td>
+                            <td className="align_center table_quantity_input">
+                                <QuantityInput
+                                    quantity={quantity}
+                                    stock={product.stock}
+                                    setQuantity={updateCart}
+                                    cartPage={true}
+                                    productId={product._id}
+                                />
+                            </td>
+                            <td>${quantity * product.price}</td>
+                            <td>
+                                <img
+                                    src={remove}
+                                    alt="remove icon"
+                                    className="cart_remove_icon"
+                                    onClick={() => removeFromCart(product._id)}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
 
-      <button className="search_button checkout_button" onClick={checkout}>
-        Checkout
-      </button>
-    </section>
-  );
+            <table className="cart_bill">
+                <tbody>
+                    <tr>
+                        <td>Subtotal</td>
+                        <td>${subTotal}</td>
+                    </tr>
+                    <tr>
+                        <td>Shipping Charge</td>
+                        <td>$5</td>
+                    </tr>
+                    <tr className="cart_bill_final">
+                        <td>Total</td>
+                        <td>${subTotal + 5}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button
+                className="search_button checkout_button"
+                onClick={checkout}
+            >
+                Checkout
+            </button>
+        </section>
+    );
 };
 
 export default memo(CartPage);
